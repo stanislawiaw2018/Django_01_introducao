bd5e3135ad3dd04692bd75f9e3bed4dc49f601ff
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-08-29 16:56:00 -0300

    Criado um modelo chamado 'Contato' e registrado o modelo administrador

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contato(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=200)
+    email = models.EmailField(max_length=50)
+    data_nascimento = models.DateField()
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
