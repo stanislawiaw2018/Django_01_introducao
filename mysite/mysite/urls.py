66caed07cba6ce419b523ab46b5830bc4dcdbc23
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-08-29 22:27:49 -0300

    Criado um novo arquivo chamado urls.py no App core.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from core import views
+from django.urls.conf import include
 
 urlpatterns = [
-    path("",views.home,name='home'),
+    path("",include('core.urls',namespace='core')),
     path('admin/', admin.site.urls),
 ]
