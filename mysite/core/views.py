4ce03e0cf302248e1991937f7ce5eb1fba51df9c
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-08-31 15:47:40 -0300

    Modificado o arquivo base.html e criado os arquivos styles.css e index.html.

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contato
 
 def home(request):
-    return render(request,"layout/base.html")
+    nome =  "Antonio Stanislaw"
+    contatos = Contato.objects.all()
+    return render(request,"layout/base.html",{'contatos': contatos})
 
 # Create your views here.
