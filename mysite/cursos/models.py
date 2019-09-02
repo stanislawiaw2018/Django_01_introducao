48f4cec9a63615e07399320d81c6b43434b382f3
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-01 23:00:01 -0300

    Modificado o arquivo models.py do app cursos; E alterado o arquivo settings.py.

--- mysite/cursos/models.py
+++ mysite/cursos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    name = models.CharField("Nome", max_length=100)
+    slug = models.SlugField("Apreviação")
+    description = models.TextField("Descrição")
+    start_date = models.DateField("Data de Inicio", null=True, blank=True)
+    image = models.ImageField(upload_to='mysite/images', verbose_name="Imagem")
+    created_at = models.DateTimeField("Criado em", auto_now_add=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True)
